328e6aaba43694c9b10ab651b31109131b0403c6
wellington-nunes <wellingtonnunez10@gmail.com>
2023-07-14 12:56:49 -0300

    feat: Adiciona o Front-end.

---
+++ resources/views/componets/navegacao.blade.php
@@ -0,0 +1,31 @@
+ <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+     <div class="position-sticky sidebar-sticky pt-3">
+         <ul class="nav flex-column">
+             <li class="nav-item">
+                 <a class="nav-link active" aria-current="page" href="#">
+                     <span data-feather="home" class="align-text-bottom"></span>
+                     Dashboard
+                 </a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">
+                     <span data-feather="file" class="align-text-bottom"></span>
+                     Vendas
+                 </a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">
+                     <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                     Produtos
+                 </a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">
+                     <span data-feather="users" class="align-text-bottom"></span>
+                     Clientes
+                 </a>
+             </li>
+         </ul>
+
+     </div>
+ </nav>
